6753ff7b8f2d572bd1b5ce7ea92174f175926166
Gavendra <pratapgavendra01@gmail.com>
2026-01-04 21:55:12 +0530

    [Gavendra] i solve a csharp access modifier Questions

---
+++ oops-csharp-practice/gcr-codebase/csharp-access-modifiers/Circle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bridge.Access_modifiers
+{
+
+
+    public class Circle
+    {
+        double radius;
+        double areaOfCircle;
+        double circumOfCircle;
+        //parameterized constructor
+        public Circle(double Radius)
+        {
+            this.radius = Radius;
+        }
+
+
+        //method to calculate area and circumference
+        void AreaAndCircumference()
+        {
+            areaOfCircle = 3.14 * (radius * radius);
+            circumOfCircle = 2 * 3.14 * radius;
+
+        }
+        //method to display area and circumference
+        void DAreaAndCircumference()
+        {
+            Console.WriteLine("Area and circumference of circle are given below");
+            Console.WriteLine("Area of Circle is " + areaOfCircle);
+            Console.WriteLine("Circumference Of Circle Is  " + circumOfCircle);
+
+        }
+        //main method
+        static void Main(string[] args)
+        {
+            Circle radius1 = new Circle(4);
+            radius1.AreaAndCircumference();
+            radius1.DAreaAndCircumference();
+
+        }
+    }
+}
+
+
