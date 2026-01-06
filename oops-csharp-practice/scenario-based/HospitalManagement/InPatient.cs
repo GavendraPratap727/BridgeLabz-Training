f600b9c312f36ab7cedf8a9cfc2283e8242d5e65
Gavendra <pratapgavendra01@gmail.com>
2026-01-06 14:51:05 +0530

    [Gavendra] i solve a opps senario bases Question Hospital Management

---
+++ oops-csharp-practice/scenario-based/HospitalManagement/InPatient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bridge.Opps_senario.HospitalManagement
+{
+     class InPatient: Patient,IPay
+    {
+        public int room;
+        public int days;
+        public double charge;
+        public double CalculateBill() {
+        return days* charge;
+        }
+        public override void Display()
+        {
+            Console.WriteLine("InPatient Details:");
+            base.Display();
+            Console.WriteLine("Romm numver "+ room);
+            Console.WriteLine("Bill " + CalculateBill());
+        }
+
+    }
+}
---
+++ oops-csharp-practice/scenario-based/HospitalManagement/OutPatient.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bridge.Opps_senario.HospitalManagement
+{
+     class OutPatient : Patient,IPay
+    {
+        public double fee;
+        public double CalculateBill() {
+            return fee;
+        }
+        public override void Display()
+        {
+            Console.WriteLine("OutPatient Details:");
+            base.Display();
+            Console.WriteLine("Fees "+ CalculateBill());
+        }
+
+    }
+}
