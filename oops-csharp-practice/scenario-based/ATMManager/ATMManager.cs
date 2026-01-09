7f120d975c439a587c2b39480a6b0a3d37e99096
Gavendra <pratapgavendra01@gmail.com>
2026-01-09 12:05:02 +0530

    [Gavendra] i solved the opps csharp senario bas question ATM Manager

---
+++ oops-csharp-practice/scenario-based/ATMManager/ATMManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bridge.Opps_senario.ATMManager
+
+{
+    class ATMManager
+    {
+        static void Main()
+        {
+            new Menu().ShowMenu();
+        }
+    }
+
+}
---
+++ oops-csharp-practice/scenario-based/ATMManager/FallbackATM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bridge.Opps_senario.ATMManager
+{
+    class FallbackATM : ATM
+    {
+        public FallbackATM() : base(new int[] { 50, 20 }) { }
+    }
+
+}
