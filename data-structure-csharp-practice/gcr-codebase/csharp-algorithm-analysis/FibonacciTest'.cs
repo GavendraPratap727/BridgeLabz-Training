75cddd101ae7c5ea0b1a2269589fe6c8afb16fc0
Gavendra <pratapgavendra01@gmail.com>
2026-01-14 15:45:24 +0530

    [Gavendra] i solved the DSA problem algo analysis

---
+++ data-structure-csharp-practice/gcr-codebase/csharp-algorithm-analysis/FibonacciTest'.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bridge.RunTime
+{
+    internal class FibonacciTest
+    {
+        static void Main()
+        {
+            int[] values = { 10, 30 };
+
+            foreach (int n in values)
+            {
+                Console.WriteLine("\nFibonacci N = " + n);
+
+                Stopwatch watch = new Stopwatch();
+
+                watch.Start();
+                int r = FibRecursive(n);
+                watch.Stop();
+                Console.WriteLine("Recursive Result: " + r);
+                Console.WriteLine("Recursive Time: " + watch.ElapsedMilliseconds + " ms");
+
+                watch.Reset();
+
+                watch.Start();
+                int i = FibIterative(n);
+                watch.Stop();
+                Console.WriteLine("Iterative Result: " + i);
+                Console.WriteLine("Iterative Time: " + watch.ElapsedMilliseconds + " ms");
+            }
+
+            Console.WriteLine("\nSkipping Recursive Fibonacci for N = 50 (Unfeasible)");
+            Stopwatch w = Stopwatch.StartNew();
+            int fast = FibIterative(50);
+            w.Stop();
+            Console.WriteLine("Iterative Result (50): " + fast);
+            Console.WriteLine("Iterative Time: " + w.ElapsedMilliseconds + " ms");
+        }
+
+        static int FibRecursive(int n)
+        {
+            if (n <= 1)
+                return n;
+            return FibRecursive(n - 1) + FibRecursive(n - 2);
+        }
+
+        static int FibIterative(int n)
+        {
+            if (n <= 1)
+                return n;
+
+            int a = 0, b = 1, sum = 0;
+
+            for (int i = 2; i <= n; i++)
+            {
+                sum = a + b;
+                a = b;
+                b = sum;
+            }
+            return b;
+        }
+    }
+}
---
+++ data-structure-csharp-practice/gcr-codebase/csharp-algorithm-analysis/SortTest.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+
+namespace bridge.RunTime
+{
+    class SortTest
+    {
+        static void Main()
+        {
+            int[] sizes = { 1000, 10000, 1000000 };
+
+            foreach (int n in sizes)
+            {
+                Console.WriteLine("Data Size: " + n);
+
+                int[] data = new int[n];
+                Random rnd = new Random();
+                for (int i = 0; i < n; i++)
+                    data[i] = rnd.Next(1, n);
+
+                if (n <= 10000)
+                {
+                    int[] bubbleArr = (int[])data.Clone();
+                    Stopwatch w1 = Stopwatch.StartNew();
+                    BubbleSort(bubbleArr);
+                    w1.Stop();
+                    Console.WriteLine("Bubble Sort Time: " + w1.ElapsedMilliseconds + " ms");
+                }
+                else
+                {
+                    Console.WriteLine("Bubble Sort Time: Unfeasible");
+                }
+
+                int[] mergeArr = (int[])data.Clone();
+                Stopwatch w2 = Stopwatch.StartNew();
+                MergeSort(mergeArr, 0, mergeArr.Length - 1);
+                w2.Stop();
+                Console.WriteLine("Merge Sort Time: " + w2.ElapsedMilliseconds + " ms");
+
+                int[] quickArr = (int[])data.Clone();
+                Stopwatch w3 = Stopwatch.StartNew();
+                QuickSort(quickArr, 0, quickArr.Length - 1);
+                w3.Stop();
+                Console.WriteLine("Quick Sort Time: " + w3.ElapsedMilliseconds + " ms");
+            }
+        }
+
+        static void BubbleSort(int[] a)
+        {
+            for (int i = 0; i < a.Length - 1; i++)
+            {
+                for (int j = 0; j < a.Length - i - 1; j++)
+                {
+                    if (a[j] > a[j + 1])
+                    {
+                        int t = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = t;
+                    }
+                }
+            }
+        }
+
+        static void MergeSort(int[] a, int l, int r)
+        {
+            if (l >= r) return;
+
+            int m = (l + r) / 2;
+            MergeSort(a, l, m);
+            MergeSort(a, m + 1, r);
+            Merge(a, l, m, r);
+        }
+
+        static void Merge(int[] a, int l, int m, int r)
+        {
+            int n1 = m - l + 1;
+            int n2 = r - m;
+
+            int[] L = new int[n1];
+            int[] R = new int[n2];
+
+            for (int i = 0; i < n1; i++) L[i] = a[l + i];
+            for (int j = 0; j < n2; j++) R[j] = a[m + 1 + j];
+
+            int i1 = 0, i2 = 0, k = l;
+
+            while (i1 < n1 && i2 < n2)
+                a[k++] = L[i1] <= R[i2] ? L[i1++] : R[i2++];
+
+            while (i1 < n1) a[k++] = L[i1++];
+            while (i2 < n2) a[k++] = R[i2++];
+        }
+
+        static void QuickSort(int[] a, int low, int high)
+        {
+            if (low < high)
+            {
+                int p = Partition(a, low, high);
+                QuickSort(a, low, p - 1);
+                QuickSort(a, p + 1, high);
+            }
+        }
+
+        static int Partition(int[] a, int low, int high)
+        {
+            int pivot = a[high];
+            int i = low - 1;
+
+            for (int j = low; j < high; j++)
+            {
+                if (a[j] < pivot)
+                {
+                    i++;
+                    int t = a[i];
+                    a[i] = a[j];
+                    a[j] = t;
+                }
+            }
+
+            int temp = a[i + 1];
+            a[i + 1] = a[high];
+            a[high] = temp;
+
+            return i + 1;
+        }
+    }
+
+}
