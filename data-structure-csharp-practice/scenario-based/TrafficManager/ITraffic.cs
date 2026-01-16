76e05eb6f70cb2281f0be902555b1c73918e0ac7
Gavendra <pratapgavendra01@gmail.com>
2026-01-16 14:56:02 +0530

    [Gavendra] i solved the DSA senario based problem TrafficManager

---
+++ data-structure-csharp-practice/scenario-based/TrafficManager/ITraffic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bridge
+{
+    interface ITraffic
+    {
+        void AddCar(string carNo);
+        void RemoveCar();
+        void ShowRoundabout();
+    }
+}
---
+++ data-structure-csharp-practice/scenario-based/TrafficManager/Roundabout.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bridge.DSA_Senario.TrafficManager
+{
+    class Roundabout : ITraffic
+    {
+        private Vehicle last;
+        public void AddCar(string carNo)
+        {
+            Vehicle newCar = new Vehicle(carNo);
+            if (last == null)
+            {
+                last = newCar;
+                last.Next = last;
+            }
+            else
+            {
+                newCar.Next = last.Next;
+                last.Next = newCar;
+                last = newCar;
+            }
+            Console.WriteLine("Car entered roundabout");
+        }
+
+        public void RemoveCar()
+        {
+            if (last == null)
+            {
+                Console.WriteLine("Roundabout empty");
+                return;
+            }
+            Vehicle first = last.Next;
+            if (first == last)
+            {
+                Console.WriteLine("Car exited: " + first.CarNumber);
+                last = null;
+            }
+            else
+            {
+                Console.WriteLine("Car exited " + first.CarNumber);
+                last.Next = first.Next;
+            }
+        }
+        public void ShowRoundabout()
+        {
+            if (last == null)
+            {
+                Console.WriteLine("No cars in roundabout");
+                return;
+            }
+            Vehicle temp = last.Next;
+            Console.Write("Roundabout Cars ");
+            do
+            {
+                Console.Write(temp.CarNumber + " ");
+                temp = temp.Next;
+            }
+            while (temp != last.Next);
+            Console.WriteLine("BACK");
+        }
+    }
+}
