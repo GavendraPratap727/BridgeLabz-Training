8245c8fc0d52659452aee4b2fa8417668f081d09
Gavendra <pratapgavendra01@gmail.com>
2026-01-19 14:53:19 +0530

    [Gavendra] i solved the DSA Senario based question 'ParcelTracker'

---
+++ data-structure-csharp-practice/scenario-based/ParcelTrackerApp/ParcelTrackerApp.cs
@@ -0,0 +1,16 @@
+﻿using bridge.DSA_Senario.ParcelTrackerApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bridge.DSA_Senario
+{
+    internal class ParcelTrackerApps
+    {
+        static void Main(string[] args) { 
+            new ParcelMenu().Start(); 
+        }
+    }
+}
