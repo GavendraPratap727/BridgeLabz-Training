639d2dd00af0183b41faf104b82fe54873d830c8
Gavendra <pratapgavendra01@gmail.com>
2026-01-21 15:48:02 +0530

    [Gavendra] i solved csharp collection senario question EduResult

---
+++ collections-csharp-practice/scenario-based/EduStudent/Menu.cs
@@ -0,0 +1,38 @@
+﻿using BridgeLabz.Collections.Senario.EduStudent;
+using System;
+
+class Menu
+{
+    public static void Start()
+    {
+        Console.Write("Enter number of students: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Student[] students = new Student[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("\nStudent " + (i + 1));
+            Console.Write("Enter name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter marks: ");
+            int marks = int.Parse(Console.ReadLine());
+
+            students[i] = new Student(name, marks);
+        }
+
+        ISortUtility sorter = new MergeSortUtility();
+        sorter.Sort(students);
+
+        Console.WriteLine("\nFinal State-wise Rank List:");
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine(
+                (i + 1) + ". " +
+                students[i].GetName() + " - " +
+                students[i].GetMarks()
+            );
+        }
+    }
+}
---
+++ collections-csharp-practice/scenario-based/EduStudent/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabz.Collections.Senario.EduStudent
+{
+    class Student
+    {
+        private string name;
+        private int marks;
+
+        public Student(string name, int marks)
+        {
+            this.name = name;
+            this.marks = marks;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public int GetMarks()
+        {
+            return marks;
+        }
+    }
+
+}
