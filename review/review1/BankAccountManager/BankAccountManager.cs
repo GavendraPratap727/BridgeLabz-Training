7e2a0bafc39413c5ac60803ef3adca1465acaf83
Gavendra <pratapgavendra01@gmail.com>
2026-02-06 11:22:18 +0530

    [Gavendra] Added review 1 and review 2

---
+++ review/review1/BankAccountManager/BankAccountManager.cs
@@ -0,0 +1,33 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace Review.BankAccountManager
+//{
+//    //    Methods – Bank Account Manager
+//    //1. Scenario: A banking app needs to perform operations like deposit, withdraw, and check balance for a user.
+//    //● Problem: Design a BankAccount class with :
+//    //● Fields/Properties: AccountNumber, Balance.
+//    //● Methods: Deposit(double), Withdraw(double), CheckBalance().
+//    //● Include logic to prevent overdraft.
+//    class BankAccountManager
+//    {
+//        static void Main(string[] args)
+//        {
+//            Console.WriteLine("Enter User Name");
+//            string name = Console.ReadLine();
+//            Console.WriteLine("Enter your Phone Number");
+//            long number = long.Parse(Console.ReadLine());
+//            Console.Write("Enter Account Number ");
+//            string accNo = Console.ReadLine();
+//            Console.Write("Enter Initial Balance ");
+//            double balance = double.Parse(Console.ReadLine());
+//            IBankAccount account = new BankOperationImpl(accNo, balance, name, number);
+//            BankMenu menu = new BankMenu(account);
+//            menu.ShowMenu();
+//        }
+//    }
+
+//}
---
+++ review/review2/RemoveNth/RemoveNth.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Review.RemoveNth
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int value)
+        {
+            data = value;
+            next = null;
+        }
+    }
+    class CustomLinkedList
+    {
+        Node head;
+        public void Add(int value)
+        {
+            Node newNode = new Node(value);
+
+            if (head == null)
+            {
+                head = newNode;
+                return;
+            }
+
+            Node temp = head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+        public void RemoveNthFromEnd(int n)
+        {
+            Node hatt= new Node(0);
+            hatt.next = head;
+            Node fast = hatt;
+            Node slow = hatt;
+
+            for (int i = 1; i <= n; i++)
+            {
+                fast = fast.next;
+            }
+
+            while (fast.next != null)
+            {
+                fast = fast.next;
+                slow = slow.next;
+            }
+            slow.next = slow.next.next;
+
+            head = hatt.next;
+        }
+        public void Print()
+        {
+            Node temp = head;
+            while (temp != null)
+            {
+                Console.Write(temp.data + "   ");
+                temp = temp.next;
+            }
+        }
+    }
+    internal class RemoveNth
+    {
+        static void Main()
+        {
+            CustomLinkedList list = new CustomLinkedList();
+            Console.WriteLine("ENter the number of number of element u want to add in the linked list");
+            int n=int.Parse(Console.ReadLine());
+            for(int i = 0; i < n; i++)
+            {
+                Console.Write("Enter the " + (i + 1) + " elemet of the list  : ");
+                list.Add(int.Parse(Console.ReadLine()));
+                Console.WriteLine();
+            }
+            Console.WriteLine("Enter which element you want to remove");
+            int remove = int.Parse(Console.ReadLine());
+            Console.WriteLine("Original linkedlist");
+            list.Print();
+            list.RemoveNthFromEnd(remove);
+            Console.WriteLine();
+            Console.WriteLine("After removing the  "+remove +"th  Node from End");
+            list.Print();
+        }
+    }
+}
