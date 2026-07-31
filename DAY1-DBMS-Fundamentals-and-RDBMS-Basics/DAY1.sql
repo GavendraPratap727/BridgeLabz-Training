670a270ce58e8917840a5619426513b4643013cf
Gavendra <pratapgavendra01@gmail.com>
2026-07-31 14:42:59 +0530

    [Gavendra] Push DAY1 Work

---
+++ DAY1-DBMS-Fundamentals-and-RDBMS-Basics/DAY1.sql
@@ -0,0 +1,31 @@
+use HealthClinic;
+
+CREATE TABLE Doctor(
+DoctorID INT IDENTITY(1,1) Primary KEy,
+Name VARCHAR(50) NOT NULL,
+Expertise VARCHAR(100) NOT NULL,
+Phone VARCHAR(15) NOT NULL UNIQUE );
+
+CREATE TABLE Patient(
+PatientID INT IDENTITY(1,1) Primary KEy,
+Name VARCHAR(50) NOT NULL,
+DOB DATE not null,
+Gender CHAR(1) Check(Gender IN('M','F','O')),
+Phone VARCHAR(15)  UNIQUE,
+PDescription  VARCHAR(500),
+Address VARCHAR(200) 
+);
+
+CREATE TABLE Appointment(
+AppointmentID int Identity(1,1) PRIMARY KEY,
+DoctorID INT NOT NULL,
+PatientID INT NOT NULL,
+AppointmentDate DATETIME NOT NULL,
+Status VARCHAR(20) DEFAULT 'Scheduled' check (STATUS in('Scheduled','Completed','Cancelled','No Show')),
+Reason VARCHAR(255),
+CONSTRAINT FDOCTOR FOREIGN KEY (DoctorID) 
+ REFERENCES Doctor(DoctorID) ON DELETE CASCADE,
+CONSTRAINT FPatient FOREIGN KEY (PatientID) 
+ REFERENCES Patient(PatientID) ON DELETE CASCADE
+);
+
---
+++ collections-csharp-practice/scenario-based_backup/SmartCheckpointApp/SmartCheckpoint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabz.Collections.Senario.SmartCheckpointApp
+{
+    internal class SmartCheckpoint
+    {
+        static void Main()
+        {
+            Menu.Start();
+        }
+    }
+}
